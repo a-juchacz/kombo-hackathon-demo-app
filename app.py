61975fc9e80531ecccfacc7c8a123d2b2ea8d5d0
Andromachi Rozaki <42938116+andromaqui@users.noreply.github.com>
2025-05-24 16:08:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,14 @@
 from flask import Flask, jsonify
 
 app = Flask(__name__)
-start_time = datetime.utcnow()
-
-@app.route('/health')
-def health():
-    now = datetime.utcnow()
-    if start_time > now:
-        return jsonify(status="healthy"), 200
-    else:
-        return jsonify(status="unhealthy"), 500
 
 @app.route('/')
 def hello():
     return "Hello from EC2!"
+
+@app.route('/health')
+def health():
+    return jsonify(status="healthy"), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80) 
