73c44e699ef3b7d10a1d93cd8f183654bbecbe2a
Andromachi Rozaki <42938116+andromaqui@users.noreply.github.com>
2025-05-24 16:22:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def hello():
 
 @app.route('/health')
 def health():
-    return jsonify(status="unhealthy"), 500
+    return jsonify(status="healthy"), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80) 
